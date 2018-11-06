cc5ce8de695edb4d347de562b721c6c20577690a
Markus Fischer <markus.fischer@uni-jena.de>
2018-11-06 13:52:01 +0100

    SSL Socket class

--- src/main/kotlin/eu/markus_fischer/unikram/mailfisch/network/Session.kt
+++ src/main/kotlin/eu/markus_fischer/unikram/mailfisch/network/Session.kt
@@ -4,12 +4,14 @@ import java.io.InputStream
 import java.io.OutputStream
 import java.lang.Exception
 import java.net.Socket
+import javax.net.ssl.SSLSocket
+import javax.net.ssl.SSLSocketFactory
 
-class Session(var remote_address: String,
+open class Session(var remote_address: String,
               var remote_port: Int)
 {
     var socket : Socket? = null
-    fun establish_connection(): Boolean {
+    open fun establish_connection(): Boolean {
         try {
             socket = Socket(remote_address, remote_port)
         } catch (e : Exception) {
@@ -20,4 +22,19 @@ class Session(var remote_address: String,
     }
 
     fun close_connection() = socket?.close()
+}
+
+class SSLSession(remote_address: String,
+                 remote_port: Int) : Session(remote_address, remote_port) {
+
+    override fun establish_connection() : Boolean {
+        try {
+            socket = SSLSocketFactory.getDefault().createSocket(remote_address, remote_port)
+            (socket as SSLSocket).startHandshake()
+        } catch (e : Exception) {
+            e.printStackTrace()
+            return false
+        }
+        return true
+    }
 }
